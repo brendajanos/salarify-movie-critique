a3af7d1521320d47fa3550e2f41863fa4baefbfc
brendajanos <brenda.janos@icloud.com>
2022-01-19 13:52:59 +0100

    basic frontend layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="App-header-leftSection">
+                    <logo>Logo</logo>
+                    <homebutton>Home</homebutton>
+                    <addbutton>Add</addbutton>
+                </div>
+                <div className="App-header-rightSection">
+                    <searchbar>Search 
+                      <button>Search</button>
+                    </searchbar>
+                </div>
+            </header>
+            <div className="App-centerArea">
+                <modal></modal>
+                <pagination></pagination>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
